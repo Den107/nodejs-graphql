6e5c7c77d9c8275114396f88b698db00d64114a0
Denis Kravchuk <deniskrv@gmail.com>
2022-05-31 23:54:57 +0300

    first commit

---
+++ routes/todo.js
@@ -0,0 +1,67 @@
+const {Router} = require('express')
+const Todo = require('../models/todo')
+const router = Router()
+
+//get todos list
+router.get('/', async (req, res) => {
+    try {
+        const todos = await Todo.findAll()
+        res.status(200).json(todos)
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
+})
+
+//create new todos
+router.post('/', async (req, res) => {
+    try {
+        const todo = await Todo.create({
+            title: req.body.title,
+            done: false
+        })
+        res.status(201).json({todo})
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
+})
+
+//change todos
+router.put('/:id', async (req, res) => {
+    try {
+        const todo = await Todo.findByPk(+req.params.id)
+        todo.done = req.body.done
+        await todo.save()
+        res.status(200).json({todo})
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
+})
+
+//delete todos
+router.delete('/:id', async (req, res) => {
+    try {
+        const todos = await Todo.findAll({
+            where: {
+                id: +req.params.id
+            }
+        })
+        await todos[0].destroy()
+        res.status(204).json({})
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
+})
+
+module.exports = router
